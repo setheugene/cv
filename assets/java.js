0578fee600050064e0de44e847f993f651e681e2
setheugene <seth.eugene@icloud.com>
2019-05-21 17:21:20 -0500

    carousel

--- assets/java.js
+++ assets/java.js
@@ -1,12 +1,21 @@
 
 
 
-$(window).on("load", function() {
-    
-// $("#neon").addClass("flicker");
+
+$(document).ready(function(){
 
     AOS.init();
+    M.AutoInit();
     
+        $('.carousel.carousel-slider').carousel({
+            fullWidth: true,
+            // pressed: true,
+            duration: .1,
+            shift: 10,
+            numVisible: 5,
+            indicators: true
+        });
+    
+  });
 
 
-})
